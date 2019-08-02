4dd131d8b6d7fe7689eb5ed59d800b827c252817
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-02 04:33:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   end 
 
   def self.new_from_filename(filename)
-    parts = filenmae.split(" - ")
+    parts = filename.split(" - ")
     artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3","")
 
     artist = Artist.find_or_create_by_name(artist_name)
